aacc33f21c87095c8d7aac6f96d5c096e8864d04
Aasim-Qureshi <aasimq194@gmail.com>
2025-11-24 17:07:24 +0530

    Added Other Locations

--- src/presentation/controllers/report.controller.js
+++ src/presentation/controllers/report.controller.js
@@ -1,4 +1,6 @@
 const { createReportUC } = require('../../application/services/report/uploadAssetsToDB.uc');
+const { reportExistenceCheckUC } = require('../../application/services/report/reportExistenceCheck.uc');
+const { addCommonFields } = require('../../application/services/report/addCommonFields.uc');
 
 const reportController = {
     async createReport(req, res) {
@@ -13,6 +15,41 @@ const reportController = {
             }
         } catch (error) {
             console.error('Error creating report:', error);
+            res.status(500).json({ success: false, message: error.message });
+        }
+    },
+    async reportExistenceCheck(req, res) {
+        try {
+            const { reportId } = req.params;
+            const { success, message, data } = await reportExistenceCheckUC(reportId);
+
+            if (success) {
+                console.log("Success", success, "message", message, "data", data);
+                res.status(200).json({ success, message });
+            } else if (message === 'Report does not exist') {
+                res.status(200).json({ success, message });
+            } else {
+                console.log("Success", success, "message", message, "data", data);
+                res.status(500).json({ success, message });
+            }
+        } catch (error) {
+            console.error('Error checking report existence:', error);
+            res.status(500).json({ success: false, message: error.message });
+        }
+    },
+
+    async addCommonFields(req, res) {
+        try {
+            const { reportId, region, city, inspectionDate } = req.body;
+            const { success, message, data } = await addCommonFields(reportId, region, city, inspectionDate);
+
+            if (success) {
+                res.status(200).json({ success, message, data });
+            } else {
+                res.status(500).json({ success, message, data });
+            }
+        } catch (error) {
+            console.error('Error adding common fields:', error);
             res.status(500).json({ success: false, message: error.message });
         }
     },
--- src/presentation/routes/report.routes.js
+++ src/presentation/routes/report.routes.js
@@ -3,6 +3,9 @@ const reportController = require('../controllers/report.controller');
 
 const router = express.Router();
 
+router.get('/reportExistenceCheck/:reportId', reportController.reportExistenceCheck);
+
 router.post('/createReport', reportController.createReport);
+router.put('/addCommonFields', reportController.addCommonFields);
 
 module.exports = router;
